753f9058ce44baceca7089a4ab7e464a3941f82f
FarronX <adityabenedictus123@gmail.com>
2019-03-15 15:56:54 +0700

    Listing 7.4

--- src/test1/CountLettersInArray.java
+++ src/test1/CountLettersInArray.java
@@ -14,7 +14,14 @@ public class CountLettersInArray {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        char[] chars = createArray();
         
+        System.out.println("The lowercase letters are:");
+        displayArrays(chars);
+        int[] counts = countLetters(chars);
+        System.out.println();
+        System.out.println("The Occurences of each letter are:");
+        displayCounts(counts);
     }
     public static char[] createArray(){
         char[] chars = new char[100];
@@ -23,6 +30,30 @@ public class CountLettersInArray {
             chars[i] = getRandomLowerCaseLetter();
         }
         return chars;
+    }
+    public static void displayArrays(char[] chars){
+        for(int i = 0; i < chars.length; i++){
+            if((i + 1) % 20 == 0)
+                System.out.println(chars[i]);
+            else
+                System.out.print(chars[i] + " ");
+        }
+    }
+    public static int[] countLetters(char[] chars){
+        int[] counts = new int[26];
+        
+        for(int i = 0; i <chars.length; i++){
+            counts[chars[i] - 'a']++;
+        }
+        return counts;
+    }
+    public static void displayCounts(int[] counts){
+        for(int i = 0; i < counts.length; i++){
+            if((i + 1) % 10 == 0)
+                System.out.println(counts[i] + " " + (char)(i + 'a'));
+            else
+                System.out.print(counts[i]+ " " + (char)(i + 'a') + " ");
+        }
     }
     public static char getRandomCharacter(){
         return getRandomCharacter('\u0000','\uFFFF');
